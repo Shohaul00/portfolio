24d8bbe9d6cd5fb4067bf2b10ed2374f5dd64b11
shohaul00 <shihabshohaul143@gmail.com>
2021-05-16 12:29:24 +0600

    updated blog section with new blogs

--- src/Configs/BlogConfig.js
+++ src/Configs/BlogConfig.js
@@ -1,27 +1,27 @@
 export const BlogConfig = [
     {
         id: 1,
-        title: "How Virtual Dom Works In React?",
-        imgURL: "https://i.ytimg.com/vi/BYbgopx44vo/maxresdefault.jpg",
+        title: "Refresh Your JavaScript Knowledge in 30 Minutes!",
+        imgURL: "https://miro.medium.com/max/2560/1*-ZkUWLhsnRSz3SkFeKpsKw.png",
         time: "2 Min",
         contentLink:
-            "https://medium.com/@shihabshohaul143/how-virtual-dom-works-in-react-6265f66e1c8e",
+            "https://shohaul.medium.com/refresh-your-javascript-knowledge-296dfcd25545",
     },
     {
         id: 2,
-        title: "How Virtual Dom Works In React?",
-        imgURL: "https://i.ytimg.com/vi/BYbgopx44vo/maxresdefault.jpg",
+        title: "A Re-Introduction To React (2021)",
+        imgURL: "https://miro.medium.com/max/700/1*nQl_rM5DRK3BsXUuole59g.png",
         time: "2 Min",
         contentLink:
-            "https://medium.com/@shihabshohaul143/how-virtual-dom-works-in-react-6265f66e1c8e",
+            "https://shohaul.medium.com/a-re-introduction-to-react-2021-4b1eb8c6f4d",
     },
     {
         id: 3,
-        title: "How Virtual Dom Works In React?",
-        imgURL: "https://i.ytimg.com/vi/BYbgopx44vo/maxresdefault.jpg",
+        title: "Top 10 JavaScript Interview Questions",
+        imgURL: "https://miro.medium.com/max/700/1*5Nzeq8Betn1IFNNqsPbrPg.jpeg",
         time: "2 Min",
         contentLink:
-            "https://medium.com/@shihabshohaul143/how-virtual-dom-works-in-react-6265f66e1c8e",
+            "https://shohaul.medium.com/top-10-javascript-interview-questions-you-should-practice-in-2021-a3fd2c2357c",
     },
     // {
     //     id: 4,
